42f4269ce83a054267dd0dfb688f37dbb5a15ea9
Labib Makarim <labibmakarim197@gmail.com>
2025-09-30 20:50:04 +0700

    feat: resume

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Typed from "typed.js";
 import "./style.css";
 import "./custom-colors.css";
 import "./header-scroll.js";
 import "./mobile-menu.js";
 import "./theme-toggle.js";
+
+AOS.init({
+  duration: 800,
+  easing: "ease-in-out",
+  once: false,
+  mirror: false,
+});
+
+const typed = new Typed("#typed-text", {
+  strings: ["Full-Stack Developer", "Tech Enthusiast"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 1500,
+  loop: true,
+});
